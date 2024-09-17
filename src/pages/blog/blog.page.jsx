7068a9ffbb6d60fb6f31234165cad960ba24f472
LoneBeast-hub <yusufoyindamola5@gmail.com>
2024-09-17 17:07:24 +0100

    working on blog page

--- src/pages/blog/blog.page.jsx
+++ src/pages/blog/blog.page.jsx
@@ -7,7 +7,23 @@ const BlogPage = () => {
 
     return(
         <div className='max-w-[1200px] py-[4rem] w-[90%] mx-auto'>
-
+            {/* blog img */}
+            <div className='w-full mb-[50px]'>
+                <img className='w-full' src={imgUrl} alt={title} />
+            </div>
+            {/* blog title */}
+            <p className='w-[90%] mx-auto text-[3.7rem] md:text-[5rem] font-bold text-center'>{title}</p>
+            {/* label containing date and author */}
+            <div className='flex gap-[10px] items-center w-fit my-[30px] mx-auto'>
+                {/* date */}
+                <p className="text-[1.4rem] font-extralight">{date.toUpperCase()}</p>
+                {/* dot */}
+                <span className='w-[3px] h-[3px] bg-black rounded-[50px] mt-[5px]'></span>
+                {/* author */}
+                <p className='text-[1.4rem]'>BY {author.toUpperCase()}</p>
+            </div>
         </div>
     )
 }
+
+export default BlogPage;
