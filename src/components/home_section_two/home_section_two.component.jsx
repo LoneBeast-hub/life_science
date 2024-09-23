80a7e8658363fd38ed865e0b28dc2b67a0bc18a7
LoneBeast-hub <yusufoyindamola5@gmail.com>
2024-09-23 14:51:20 +0100

    made changes to the home page second section

--- src/components/home_section_two/home_section_two.component.jsx
+++ src/components/home_section_two/home_section_two.component.jsx
@@ -1,86 +1,130 @@
-import React, { useEffect, useState, useRef } from 'react';
-import './home_section_two.style.css';
+import EV from '../../assets/expertise.jpg';
+import TSV from '../../assets/tailored_solutions.jpg';
+import IDV from '../../assets/innovation_driven.jpg';
+import CV from '../../assets/commitment.jpg';
 
 const HomeSectionTwo = () => {
-    const [countries, setCountries] = useState(0);
-    const [cities, setCities] = useState(0);
-    const [clients, setClients] = useState(0);
-    const [areas, setAreas] = useState(0);
-    const ref = useRef(null);
-
-    const incrementNumber = (start, end, setter, duration = 2000) => {
-        let startTime = null;
-        const step = (timestamp) => {
-            if (!startTime) startTime = timestamp;
-            const progress = timestamp - startTime;
-            const current = Math.min(Math.floor((progress / duration) * (end - start) + start), end);
-            setter(current);
-            if (progress < duration) {
-                window.requestAnimationFrame(step);
-            }
-        };
-        window.requestAnimationFrame(step);
-    };
-
-    useEffect(() => {
-        const observer = new IntersectionObserver(
-            (entries) => {
-                const [entry] = entries;
-                if (entry.isIntersecting) {
-                    incrementNumber(0, 9, setCountries);
-                    incrementNumber(0, 35, setCities);
-                    incrementNumber(0, 30, setClients);
-                    incrementNumber(0, 200, setAreas);
-                    observer.disconnect(); // Stop observing after numbers have been animated
-                }
-            },
-            { threshold: 0.5 } // Trigger when 50% of the component is in view
-        );
-
-        if (ref.current) {
-            observer.observe(ref.current);
-        }
-
-        return () => {
-            if (ref.current) {
-                observer.disconnect();
-            }
-        };
-    }, []);
 
     return (
-        <div ref={ref} className="w-full flex py-[40px] items-center relative min-h-[418px] home_section_two_bg bg-cover bg-fixed bg-no-repeat">
+        <div className="w-full flex py-[40px] items-center relative min-h-[418px]">
             <div className="max-w-[1200px] w-[90%] mx-auto">
                 <div>
                     {/* heading */}
-                    <p className='text-[2.4rem] text-center md:text-[3.7rem] font-bold text-white'>Our Work</p>
-                    {/* sub heading */}
-                    <p className='text-[1.6rem] text-center text-white'>Our work is global</p>
-                    {/* flex */}
-                    <div className="flex flex-col mt-[4rem] md:justify-between gap-[2rem] md:flex-row">
-                        <div className='flex flex-col text-white items-center justify-center'>
-                            <p className='text-[1.6rem]'>We've worked in</p>
-                            {/* count */}
-                            <p className='text-[6rem] font-bold leading-[7rem]'>{countries}</p>
-                            <p className='text-[1.6rem]'>Countries</p>
+                    <p className='text-[2.4rem] text-center md:text-[3.7rem] font-bold text-black mb-[30px]'>Expertise</p>
+                    {/* reasons */}
+                    <div className="grid lg:grid-cols-3 gap-[25px] sm:grid-cols-2 grid-cols-1">
+                        {/* Engineering */}
+                        <div className="shadow-[0_0_30px_5px_#dbdbdb] p-[20px] rounded-xl">
+                            {/* vector img */}
+                            <div className='w-full rounded-xl overflow-hidden border border-[#dbdbdb]'>
+                                <img className='w-full' src={EV} alt="Engineering Vector" />
+                            </div>
+                            {/* info */}
+                            <div className='text-center mt-[10px]'>
+                                {/* heading */}
+                                <p className='text-[2.4rem] font-semibold'>Engineering</p>
+                                {/* info */}
+                                <p className='text-[1.6rem]'>
+                                    We specialize in innovative solutions through Regenerative Medicine, 
+                                    Tissue Engineering, and Medical Device Development, advancing healthcare 
+                                    with cutting-edge technology.
+                                </p>
+                            </div>
                         </div>
-                        <div className='flex flex-col text-white items-center justify-center'>
-                            <p className='text-[1.6rem]'>In</p>
-                            {/* count */}
-                            <p className='text-[6rem] font-bold leading-[7rem]'>{cities}</p>
-                            <p className='text-[1.6rem]'>Cities</p>
+                        {/* AI in Medicine */}
+                        <div className="shadow-[0_0_30px_5px_#dbdbdb] p-[20px] rounded-xl">
+                            {/* vector img */}
+                            <div className='w-full rounded-xl overflow-hidden border border-[#dbdbdb]'>
+                                <img className='w-full' src={TSV} alt="AI in Medicine Vector" />
+                            </div>
+                            {/* info */}
+                            <div className='text-center mt-[10px]'>
+                                {/* heading */}
+                                <p className='text-[2.4rem] font-semibold'>AI in Medicine</p>
+                                {/* info */}
+                                <p className='text-[1.6rem]'>
+                                    AI in Medicine is transforming healthcare with innovations in medical 
+                                    imaging, diagnostics, predictive analysis, and decision support. We 
+                                    harness natural language processing to streamline patient data and 
+                                    develop personalized treatment plans for improved patient outcomes.
+                                </p>
+                            </div>
                         </div>
-                        <div className='flex flex-col text-white items-center justify-center'>
-                            <p className='text-[1.6rem]'>Across</p>
-                            {/* count */}
-                            <p className='text-[6rem] font-bold leading-[7rem]'>{clients}</p>
-                            <p className='text-[1.6rem]'>Clients</p>
+                        {/* Clinical Research */}
+                        <div className="shadow-[0_0_30px_5px_#dbdbdb] p-[20px] rounded-xl">
+                            {/* vector img */}
+                            <div className='w-full rounded-xl overflow-hidden border border-[#dbdbdb]'>
+                                <img className='w-full' src={IDV} alt="Clinical Research Vector" />
+                            </div>
+                            {/* info */}
+                            <div className='text-center mt-[10px]'>
+                                {/* heading */}
+                                <p className='text-[2.4rem] font-semibold'>Clinical Research</p>
+                                {/* info */}
+                                <p className='text-[1.6rem]'>
+                                    Our expertise spans Regulatory Affairs, Clinical Trial Management, 
+                                    Pharmacovigilance, Biostatistics, and Clinical Project Management. 
+                                    We ensure compliance, safety, and success in clinical trials.
+                                </p>
+                            </div>
                         </div>
-                        <div className='flex flex-col text-white items-center justify-center'>
-                            <p className='text-[1.6rem]'>In over</p>
-                            {/* count */}
-                            <p className='text-[6rem] font-bold leading-[7rem]'>{areas}</p>
-                            <p className='text-[1.6rem]'>Therapeutic areas</p>
+                        {/* Biosciences */}
+                        <div className="shadow-[0_0_30px_5px_#dbdbdb] p-[20px] rounded-xl">
+                            {/* vector img */}
+                            <div className='w-full rounded-xl overflow-hidden border border-[#dbdbdb]'>
+                                <img className='w-full' src={CV} alt="Biosciences Vector" />
+                            </div>
+                            {/* info */}
+                            <div className='text-center mt-[10px]'>
+                                {/* heading */}
+                                <p className='text-[2.4rem] font-semibold'>Biosciences</p>
+                                {/* info */}
+                                <p className='text-[1.6rem]'>
+                                    Biosciences expertise covers Molecular Biology, Microbiology, 
+                                    Immunology, Cell Biology, and Genomics/Bioinformatics, offering 
+                                    advanced research and solutions in these critical areas of life 
+                                    sciences.
+                                </p>
+                            </div>
+                        </div>
+                        {/* Health Sciences */}
+                        <div className="shadow-[0_0_30px_5px_#dbdbdb] p-[20px] rounded-xl">
+                            {/* vector img */}
+                            <div className='w-full rounded-xl overflow-hidden border border-[#dbdbdb]'>
+                                <img className='w-full' src={CV} alt="Health Sciences" />
+                            </div>
+                            {/* info */}
+                            <div className='text-center mt-[10px]'>
+                                {/* heading */}
+                                <p className='text-[2.4rem] font-semibold'>Health Sciences</p>
+                                {/* info */}
+                                <p className='text-[1.6rem]'>
+                                    Our expertise spans various fields in health sciences, focusing 
+                                    on advancing healthcare through innovative research and application. 
+                                    From disease prevention and treatment to health promotion and wellness, 
+                                    we work to improve public health outcomes through scientific inquiry, 
+                                    clinical practice, and policy development.
+                                </p>
+                            </div>
+                        </div>
+                        {/* Business */}
+                        <div className="shadow-[0_0_30px_5px_#dbdbdb] p-[20px] rounded-xl">
+                            {/* vector img */}
+                            <div className='w-full rounded-xl overflow-hidden border border-[#dbdbdb]'>
+                                <img className='w-full' src={CV} alt="Business" />
+                            </div>
+                            {/* info */}
+                            <div className='text-center mt-[10px]'>
+                                {/* heading */}
+                                <p className='text-[2.4rem] font-semibold'>Business</p>
+                                {/* info */}
+                                <p className='text-[1.6rem]'>
+                                    We integrate business acumen with life sciences and medTech innovations 
+                                    to drive growth and efficiency. Our focus is on leveraging market 
+                                    insights and strategic partnerships to enhance healthcare solutions 
+                                    and improve patient outcomes.
+                                </p>
+                            </div>
                         </div>
                     </div>
                 </div>
